ae28db415c247fe1617bac3b96c59c75f18fc64a
Tanya Engelhart <tkengelhart@gmail.com>
2021-05-23 11:59:54 -0500

    working with chakra

--- src/components/App/App.jsx
+++ src/components/App/App.jsx
@@ -1,5 +1,4 @@
 import React from 'react';
-import axios from 'axios';
 import './App.css';
 import Header from '../Header/Header';
 import Home from '../Home/Home'
@@ -10,6 +9,7 @@ import SupportForm from '../Forms/SupportForm';
 import UnderstandingForm from '../Forms/UnderstandingForm';
 import Review from '../Forms/Review';
 import { HashRouter as Router, Route, Switch, Link, NavLink } from 'react-router-dom';
+
 // import { useState } from 'react';
 // import { useSelector } from 'react-redux';
 
@@ -18,22 +18,24 @@ import { HashRouter as Router, Route, Switch, Link, NavLink } from 'react-router
 function App() {
 
   return (
+    <>
+      <Router>
+        <div>
+          <Header />
+          <hr />
+          <switch>
+            <Route exact path='/'><Home /></Route>
+            <Route path='/feelings'><FeelingForm /></Route>
+            <Route path='/understanding'><UnderstandingForm /></Route>
+            <Route path='/support'><SupportForm /></Route>
+            <Route path='/comments'><CommentsForm /></Route>
+            <Route path='/review'><Review /></Route>
+            <Route path='/submit'><Submission /></Route>
+          </switch>
 
-    <Router>
-      <div className='App'>
-        <Header />
-        <hr />
-        <switch>
-          <Route exact path='/'><Home /></Route>
-          <Route path='/feelings'><FeelingForm /></Route>
-          <Route path='/understanding'><UnderstandingForm /></Route>
-          <Route path='/support'><SupportForm /></Route>
-          <Route path='/comments'><CommentsForm /></Route>
-          <Route path='/review'><Review /></Route>
-          <Route path='/submit'><Submission /></Route>
-        </switch>
-      </div>
-    </Router>
+        </div>
+      </Router>
+    </>
   );
 }
 
