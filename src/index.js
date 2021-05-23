d38d6f32cc9fabe0c39852c7a920576c8b29324d
Tanya Engelhart <tkengelhart@gmail.com>
2021-05-23 14:20:58 -0500

    dispatch reset works now

--- src/index.js
+++ src/index.js
@@ -23,49 +23,44 @@ import registerServiceWorker from './registerServiceWorker';
 //     )
 // }
 
+
 const feelingReducer = (state = 0, action) => {
-    if (action.type === 'ADD_FEELING')
-        return action.payload;
-    else if (action.type === 'RESET')
-        return state;
+    if (action.type === 'ADD_FEELING') {
+        return action.payload
+    } else if (action.type === 'RESET') {
+        let newState = 0;
+        state = newState;
+    }
+    return state;
 }
-
-
 
 const understandReducer = (state = 0, action) => {
-    if (action.type === 'ADD_UNDERSTAND') return action.payload;
+    if (action.type === 'ADD_UNDERSTAND') {
+        return action.payload
+    } else if (action.type === 'RESET') {
+        let newState = 0;
+        state = newState;
+    }
     return state;
-
 }
 const supportReducer = (state = 0, action) => {
-    if (action.type === 'ADD_SUPPORT') return action.payload;
-    return state;
-
-}
-
-const commentsReducer = (state = [], action) => {
-    if (action.type === 'ADD_COMMENTS') return action.payload;
-    return state;
-}
-
-
-// const reviewReducer = (state = {}, action) => {
-//     if (action.type === 'DISPLAY_REVIEW') {
-//         return action.payload;
-//     }
-//     let newState = [...state, action.payload];
-//     return newState;
-// }
-
-const submissionReducer = (state = {}, action) => {
-    if (action.type === 'SUBMIT') {
-        let newState = [...state, action.payload];
-        return newState,
-            alert('Success!');
+    if (action.type === 'ADD_SUPPORT') {
+        return action.payload
+    } else if (action.type === 'RESET') {
+        let newState = 0;
+        state = newState;
     }
-    applyMiddleware(logger)
+    return state;
 }
-
+const commentsReducer = (state = [], action) => {
+    if (action.type === 'ADD_COMMENTS') {
+        return action.payload
+    } else if (action.type === 'RESET') {
+        let newState = [];
+        state = newState;
+    }
+    return state;
+}
 
 const storeInstance = createStore(
     combineReducers({
@@ -73,16 +68,10 @@ const storeInstance = createStore(
         understandReducer,
         supportReducer,
         commentsReducer,
-        // reviewReducer,
-        // submissionReducer
     }),
     applyMiddleware(logger)
 
 );
-
-
-
-
 
 
 
@@ -95,4 +84,23 @@ ReactDOM.render(
     // </ChakraProvider>
 );
 registerServiceWorker();
+//not going to use this one
+// const reviewReducer = (state = {}, action) => {
+//     if (action.type === 'DISPLAY_REVIEW') {
+//         return action.payload;
+//     }
+//     let newState = [...state, action.payload];
+//     return newState;
+// }
+
+
+//not longer using this one either
+
+// const submissionReducer = (state = {}, action) => {
+//     if (action.type === 'SUBMIT') {
+//         let newState = [...state, action.payload];
+//         return newState,
+//             alert('Success!');
+//     }
+// }
 
